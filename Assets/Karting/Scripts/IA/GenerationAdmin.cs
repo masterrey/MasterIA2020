9ab990ad51e42445ca5f4f7cdc389535bad997f1
Reinaldo Ramos <masterrey@gmail.com>
2020-03-26 20:52:35 -0300

    IA e gerador

---
+++ Assets/Karting/Scripts/IA/GenerationAdmin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerationAdmin : MonoBehaviour
+{
+    public GameObject iaprefab;
+    public int karts=10;
+    public IAInput[] iaKarts;
+    float[] bestiaturnDNA;
+    void Start()
+    {
+        iaKarts = new IAInput[karts];
+        for (int i = 0; i < karts; i++)
+        {
+            GameObject kart=
+            Instantiate(iaprefab, transform.position, transform.rotation);
+            iaKarts[i] = kart.GetComponent<IAInput>();
+            if (bestiaturnDNA != null)
+            {
+                iaKarts[i].SetDNA(bestiaturnDNA);
+            }
+            else
+            {
+                iaKarts[i].CreateDNA();
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Karting/Scripts/IA/IAInput.cs
+++ Assets/Karting/Scripts/IA/IAInput.cs
@@ -1,4 +1,5 @@
 ﻿using KartGame.KartSystems;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -30,21 +31,49 @@ public class IAInput : MonoBehaviour , IInput
         get { return m_HopHeld; }
     }
 
+    internal void SetDNA(float[] bestiaturnDNA)
+    {
+        iaturnDNA = bestiaturnDNA;
+    }
+
     float m_Acceleration;
+    [SerializeField]
     float m_Steering;
     bool m_HopPressed;
     bool m_HopHeld;
     bool m_BoostPressed;
     bool m_FirePressed;
-
+    [SerializeField]
+    float[] iaturnDNA;
+    float timeindex;
+    int checkpoint;
     void Start()
     {
-        
+        m_Acceleration = 1;
+    }
+
+    internal void CreateDNA()
+    {
+        iaturnDNA = new float[37];
+        for (int i = 0; i < iaturnDNA.Length; i++)
+        {
+            iaturnDNA[i] = UnityEngine.Random.Range(-1, 2);
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //timeindex += Time.deltaTime*2;
+        m_Steering = iaturnDNA[checkpoint];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("CheckPointIA"))
+        {
+            checkpoint++;
+        }
     }
 }
